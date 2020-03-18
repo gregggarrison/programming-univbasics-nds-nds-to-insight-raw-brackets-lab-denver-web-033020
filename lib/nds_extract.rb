b90a69d9e14731c3586d04ebdb7a991eccab9bc7
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-18 03:04:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,17 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  def directors_totals(nds)
+  result = {}
+  nil
+
+  director_index = 0
+  while director_index < nds.length do
+    current_director = nds[director_index][:name]
+    result[current_director] = gross_for_director( nds[director_index] )
+    director_index += 1
+  end
+  result
+end
   
   
   
@@ -23,6 +33,3 @@ def directors_totals(nds)
   # ...
   #
   #
-  # Be sure to return the result at the end!
-  nil
-end
